faa64892eb9b3a943b2ed47e4a7ae335b332da24
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-10-26 00:03:22 +0530

    login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
+const {BadRequestError, UnauthenticatedError} = require('../errors')
 
 const register = async (req, res) => {
 
@@ -11,7 +11,28 @@ const register = async (req, res) => {
     .json({user: {name:user.name}, token})
 }
 const login = async (req, res) => {
-    res.send('login user')
+    const {email, password} = req.body
+
+    // if(!email || !password){
+    //     throw new BadRequestError("Please provide valid credentials")
+    // }
+
+    //now if the error handling is passed, we move on to find the email of the user
+    const user = await User.findOne({email})
+    
+    if(!user){
+        throw new UnauthenticatedError('Invalid credentials')
+    }
+    
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid password')
+    }
+
+    //compare password 
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({user:{user:user.name}, token})
+
 }
 
 module.exports = {
